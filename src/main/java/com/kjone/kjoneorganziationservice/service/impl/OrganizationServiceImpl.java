6b92615d9cc163644a327cd44be7c1f6eb319db0
kjone8371 <rlawjddnsj8371@gmail.com>
2024-09-29 15:00:30 +0900

    조직 생성 관리 추가

--- src/main/java/com/kjone/kjoneorganziationservice/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/kjone/kjoneorganziationservice/service/impl/OrganizationServiceImpl.java
@@ -1,17 +1,21 @@
 package com.kjone.kjoneorganziationservice.service.impl;
 
 
+import com.kjone.kjoneorganziationservice.domain.organization.Organization;
 import com.kjone.kjoneorganziationservice.domain.organization_user.LoginRequest;
 import com.kjone.kjoneorganziationservice.domain.organization_user.Organization_User;
 import com.kjone.kjoneorganziationservice.domain.request.SignRequest;
 import com.kjone.kjoneorganziationservice.domain.response.SignResponse;
 import com.kjone.kjoneorganziationservice.domain.role.Authority;
+import com.kjone.kjoneorganziationservice.exception.GlobalException;
 import com.kjone.kjoneorganziationservice.repository.OrganizationRepository;
+import com.kjone.kjoneorganziationservice.repository.OrganizationUserRepository;
 import com.kjone.kjoneorganziationservice.security.cookie.CookieProvider;
 import com.kjone.kjoneorganziationservice.service.OrganizationService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,6 +31,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrganizationServiceImpl implements OrganizationService {
     private final OrganizationRepository organizationRepository;
+    private final OrganizationUserRepository organizationUserRepository;
     private final PasswordEncoder passwordEncoder;
     private final CookieProvider cookieProvider;
 
@@ -36,7 +41,7 @@ public class OrganizationServiceImpl implements OrganizationService {
         try {
 
             // 이메일 중복 확인
-            Optional<Organization_User> existingUser = organizationRepository.findByEmail(signRequest.getEmail());
+            Optional<Organization_User> existingUser = organizationUserRepository.findByEmail(signRequest.getEmail());
             if (existingUser.isPresent()) {
                 throw new Exception("이미 사용 중인 이메일입니다.");
             }
@@ -49,7 +54,7 @@ public class OrganizationServiceImpl implements OrganizationService {
                     .image(signRequest.getImage())
                     .roles(Collections.singleton(Authority.OWNER)) // roles 필드를 설정합니다.
                     .build();
-            organizationRepository.save(organization_user);
+            organizationUserRepository.save(organization_user);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw new Exception("이미 사용 중인 이메일 입니다.");
@@ -60,7 +65,7 @@ public class OrganizationServiceImpl implements OrganizationService {
     // 로그인 메서드
     @Override
     public SignResponse Organization_signIn(LoginRequest loginRequest) throws Exception {
-        Organization_User organization_user = organizationRepository.findByEmail(loginRequest.getEmail())
+        Organization_User organization_user = organizationUserRepository.findByEmail(loginRequest.getEmail())
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
 
@@ -83,21 +88,21 @@ public class OrganizationServiceImpl implements OrganizationService {
 
     @Override
     public Organization_User findByOrgEmail(String email) {
-        return organizationRepository.findByEmail(email).orElse(null); // 사용자 없을 경우 null 반환
+        return organizationUserRepository.findByEmail(email).orElse(null); // 사용자 없을 경우 null 반환
     }
 
     @Override
     @Transactional
     public void deleteOrgByEmail(String email) throws Exception {
-        Organization_User organization_user = organizationRepository.findByEmail(email)
+        Organization_User organization_user = organizationUserRepository.findByEmail(email)
                 .orElseThrow(() -> new Exception("사용자를 찾을 수 없습니다."));
-        organizationRepository.delete(organization_user);
+        organizationUserRepository.delete(organization_user);
     }
 
     @Override
     public List<Organization_User> getAllUsers() {
 
-        return organizationRepository.findAll().stream()
+        return organizationUserRepository.findAll().stream()
                 .map(organization_user -> new Organization_User(
                         organization_user.getId(),
                         organization_user.getEmail(),
@@ -115,7 +120,7 @@ public class OrganizationServiceImpl implements OrganizationService {
     @Override
     @Transactional
     public Organization_User createProfile(Long id, SignRequest signRequest) throws Exception {
-        Organization_User organization_user = organizationRepository.findById(id)
+        Organization_User organization_user = organizationUserRepository.findById(id)
                 .orElseThrow(() -> new Exception("사용자를 찾을 수 없습니다."));
 
         organization_user.setName(signRequest.getName());
@@ -123,13 +128,13 @@ public class OrganizationServiceImpl implements OrganizationService {
         organization_user.setImage(signRequest.getImage());
         organization_user.setUpdateTime(LocalDateTime.now());
 
-        return organizationRepository.save(organization_user);
+        return organizationUserRepository.save(organization_user);
     }
 
     @Override
     @Transactional
     public Organization_User updateProfile(Long id, SignRequest signRequest) throws Exception {
-        Organization_User organization_user = organizationRepository.findById(id)
+        Organization_User organization_user = organizationUserRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
         organization_user.setName(signRequest.getName());
@@ -137,8 +142,46 @@ public class OrganizationServiceImpl implements OrganizationService {
         organization_user.setImage(signRequest.getImage());
         organization_user.setUpdateTime(LocalDateTime.now());
 
-        return organizationRepository.save(organization_user);
+        return organizationUserRepository.save(organization_user);
     }
+
+
+    // 조직 생성 관리
+    @Override
+    @Transactional(rollbackFor = {GlobalException.class})
+    public void createOrganization(Organization organization, String sender){
+
+        // 조직 사용자 확인
+        Organization_User user = organizationUserRepository.findByEmail(sender)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        // 사용자 권한 확인
+        if (!user.isOwner()) {
+            throw new GlobalException(HttpStatus.FORBIDDEN, "조직 생성 권한이 없습니다.");
+        }
+
+        // 조직 이름 유효성 검사
+        if (organization.getName().length() < 3) {
+            throw new GlobalException(HttpStatus.BAD_REQUEST, "조직명은 2자 이상이어야 합니다.");
+        } else if (organization.getName().length() > 50) {
+            throw new GlobalException(HttpStatus.BAD_REQUEST, "조직명은 50자를 넘길 수 없습니다.");
+        }
+
+        // 조직 생성
+        Organization newOrganization = Organization.builder()
+                .name(organization.getName()) // 조직 이름 설정
+                .owner_id(user) // 조직자 설정
+                .build();
+
+        organizationRepository.save(newOrganization);
+
+        // 조직 생성 성공 메시지 로그 또는 처리
+        System.out.println("조직이 성공적으로 생성되었습니다.");
+
+
+
+    }
+
 
 
 
