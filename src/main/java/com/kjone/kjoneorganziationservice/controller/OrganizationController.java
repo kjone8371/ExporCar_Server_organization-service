a226ed3e238e18c87f8d9ab1adfc0a18018d3060
kjone8371 <rlawjddnsj8371@gmail.com>
2024-09-30 14:04:05 +0900

    조직 생성/관리 컨트롤러 수정 중

--- src/main/java/com/kjone/kjoneorganziationservice/controller/OrganizationController.java
+++ src/main/java/com/kjone/kjoneorganziationservice/controller/OrganizationController.java
@@ -1,10 +1,16 @@
 package com.kjone.kjoneorganziationservice.controller;
 
 
+import com.kjone.kjoneorganziationservice.domain.organization.Organization;
+import com.kjone.kjoneorganziationservice.repository.OrganizationRepository;
 import com.kjone.kjoneorganziationservice.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 // 조직도 생성/관리
@@ -13,6 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrganizationController {
     private final OrganizationService organizationService;
+    private final OrganizationRepository organizationRepository;
+
+
+    
+
+
 
 
 
