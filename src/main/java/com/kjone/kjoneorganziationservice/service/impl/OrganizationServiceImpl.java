7e3cef43b7f1c5cd01a8d5c70e247463c7562342
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-04 10:51:36 +0900

    조직 서비스레이어

--- src/main/java/com/kjone/kjoneorganziationservice/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/kjone/kjoneorganziationservice/service/impl/OrganizationServiceImpl.java
@@ -26,7 +26,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-dwa
+
 @Service
 @RequiredArgsConstructor
 public class OrganizationServiceImpl implements OrganizationService {
